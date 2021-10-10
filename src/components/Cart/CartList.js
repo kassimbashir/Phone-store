49cbbb322df080a7cdac50a99af37aa550efc495
kassim <kassimbashir@gmail.com>
2021-10-11 01:25:49 +0300

    move to new repo

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import CartItem from "./CartItem";
 export default function CartList({ value }) {
-  console.log(value);
   const { cart } = value;
   return (
     <div className="container-fluid">
